b6da2ca2ef48702c5806e0f63d8872299b9fa462
robert <myhren@gmail.com>
2019-11-05 08:26:08 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Dropzone from "react-dropzone";
+import Papa from "papaparse";
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+import generateOfx from "./ofx";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ofx: "",
+            errors: [],
+            done: false,
+            account: 'Roberts konto'
+        };
+
+        this.dropdown = [
+          'Roberts konto', 'Felleskonto', 'Randis konto'
+        ]
+    }
+
+
+
+    handleDrop = (files) => {
+        Papa.parse(files[0], {
+            header: false,
+            delimiter: ";",
+            complete: (results) => {
+                var arrOfx = generateOfx(results.data, this.state.account);
+                this.setState({
+                    ofx: arrOfx['header'],
+                    errors: results.errors
+                });
+
+                const link = document.createElement('a');
+                link.download = arrOfx['bankid'] + '_' + arrOfx['account'] + '_' + arrOfx['sDate'] + '_' + arrOfx['eDate'] + '.ofx';
+                const blob = new Blob([arrOfx['header']], {type: 'text/plain'});
+                link.href = window.URL.createObjectURL(blob);
+                //link.click();
+            },
+        });
+    }
+
+    handleChange = (event) => {
+        this.setState({ account: event.value });
+        console.log(this.state.account)
+      };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Arendal & Omegn Sparekasse CSV to YNAB OFX converter</h1>
+                </div>
+                <Dropdown options={this.dropdown} onChange={this.handleChange} value={this.dropdown[0]} placeholder="Velg en konto" />
+
+                <Dropzone onDrop={acceptedFiles => this.handleDrop(acceptedFiles)}>
+                {({getRootProps, getInputProps}) => (
+    <section>
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </section>
+  )}
+</Dropzone>
+
+                <div className="errors">
+                    {this.state.errors.map(error => <p key={error.row + error.code}>
+                        Warning: error line {error.row} [{error.code}]: {error.message}
+                    </p>)}
+                </div>
+
+                <pre className="ofx-preview">
+                    {this.state.ofx || "No results yet"}
+                </pre>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,17 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Uploader', () => {
+  test('uploads file', async () => {
+    const rows = ['NAME,ADDRESS,ZIP', 'james,1800 sunny ln,40000', 'ronda,1200 peaches ln,50000']
+    const file = new File([rows.join('\n')], 'some.csv')
+    const { getByText } = render(<Uploader/>)
+    const formElement = getByText('Drop File');
+    Object.defineProperty(formElement, 'files', { value: [file] });
+    fireEvent.drop(formElement)
+    await waitForElement(() => getByText('Success'))
+
+    expect(getByText('Your file has been uploaded!')).toBeDefined()
+  })
+})
